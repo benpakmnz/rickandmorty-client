9de9893dfe746868135e275cb4212e4ec42d015b
benny <benpakmnz@gmail.com>
2024-04-21 12:09:16 +0300

    handle location request by admin

--- src/pages/locations/Locations.tsx
+++ src/pages/locations/Locations.tsx
@@ -1,20 +1,40 @@
-import { Button, Grid, InputBase, Paper, Typography } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Grid,
+  InputBase,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Paper,
+  Typography,
+} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 import React, { useState } from "react";
 import LocationItem from "../../components/locationItem/LocationItem";
 import { keepPreviousData, useMutation, useQuery } from "@tanstack/react-query";
 import { getLocations } from "../../services/apiLocation";
-import { addLocationRequest } from "../../services/apiLocationRequests";
+import {
+  addLocationRequest,
+  deleteLocationRequests,
+  getAllLocationRequests,
+} from "../../services/apiLocationRequests";
 import useDebounce from "../../hooks/useDebounce";
 import styles from "./styles.module.scss";
 import Loader from "../../components/loader/Loader";
-import { ILocationParams } from "../../utils/interfaces";
+import { ILocationParams, IUserParams } from "../../utils/interfaces";
+import { queryClient } from "../../main";
 
 const Locations: React.FC = () => {
   const [searchInput, setSearchInput] = useState<string>("");
+
   const [successMessage, setSuccessMessage] = useState("");
   const debouncedSearchValue = useDebounce(searchInput, 500);
+  const loggedUser: IUserParams | undefined = queryClient.getQueryData([
+    "user",
+  ]);
 
   const { data: locationsList, isLoading } = useQuery<ILocationParams[]>({
     queryKey: ["locations", debouncedSearchValue],
@@ -25,62 +45,120 @@ const Locations: React.FC = () => {
     placeholderData: keepPreviousData,
   });
 
-  const mutation = useMutation({
+  const {
+    data: requestsList,
+    isLoading: isLoadingRequsets,
+    refetch: refetchRequestsList,
+  } = useQuery<{ location: string; id: string }[]>({
+    queryKey: ["requestsList"],
+    queryFn: async () => {
+      const data = await getAllLocationRequests();
+      return data;
+    },
+    enabled: loggedUser?.isAdmin,
+  });
+
+  const locationsMutation = useMutation({
     mutationFn: (data: string) => addLocationRequest(data),
     onSuccess: () => {
       setSuccessMessage("Your requset has been submitted, thank you ");
+      refetchRequestsList();
+    },
+  });
+
+  const requestlocationsMutation = useMutation({
+    mutationFn: (id: string) => deleteLocationRequests(id),
+    onSuccess: () => {
+      refetchRequestsList();
     },
   });
 
   const handleButtonClick = async () => {
-    mutation.mutate(searchInput);
+    locationsMutation.mutate(searchInput);
   };
 
   const handleInputChange = (value: string) => {
     setSearchInput(value);
   };
 
+  console.log(requestsList);
+
   return (
-    <Grid container rowSpacing={8} mt={1} className={styles.locationWrapper}>
-      <Grid item xs={9} lg={8} xl={6}>
-        <Paper className={styles.searchbar}>
-          <InputBase
-            className={styles.inputBase}
-            placeholder="Search for location"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              handleInputChange(e.target.value)
-            }
-          />
-          <SearchIcon />
-        </Paper>
-      </Grid>
-      <Grid item xs={10}>
-        {isLoading && <Loader />}
-        <Grid container spacing={2} justifyContent="left" alignItems="center">
-          {locationsList && locationsList.length > 0 ? (
-            locationsList?.map((location) => (
-              <Grid item xs={6} lg={4} xl={3}>
-                <LocationItem location={location} key={location.id} />
-              </Grid>
-            ))
-          ) : (
-            <Grid item className={styles.noLocationsWrapper}>
-              <Typography variant="h4" mb={3}>
-                Couldn't find what you're looking for? Don't worry!
-                <br /> Let us know, and we'll hunt it down for you.
-              </Typography>
-              <Button variant="contained" onClick={handleButtonClick}>
-                Request Location Addition
-              </Button>
-              {successMessage && (
-                <Typography variant="body1">{successMessage}</Typography>
+    <Grid container p={8}>
+      {loggedUser?.isAdmin && (
+        <Grid item xs={12} lg={2}>
+          <Typography sx={{ mb: 2 }} variant="h6" component="div">
+            Location Requests ({requestsList?.length || 0})
+          </Typography>
+          <List>
+            {requestsList?.map((request) => (
+              <ListItem
+                divider
+                key={request.id}
+                sx={{ bgcolor: "background.paper" }}
+              >
+                <ListItemAvatar>
+                  <Avatar></Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={request.location} />
+                <Button
+                  onClick={() => requestlocationsMutation.mutate(request.id)}
+                >
+                  x
+                </Button>
+              </ListItem>
+            ))}
+          </List>
+        </Grid>
+      )}
+      <Grid item xs={12} lg={loggedUser?.isAdmin ? 10 : 12}>
+        <Grid container rowSpacing={8} className={styles.locationWrapper}>
+          <Grid item xs={9} lg={8} xl={6}>
+            <Paper className={styles.searchbar}>
+              <InputBase
+                className={styles.inputBase}
+                placeholder="Search for location"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  handleInputChange(e.target.value)
+                }
+              />
+              <SearchIcon />
+            </Paper>
+          </Grid>
+          <Grid item xs={10}>
+            {isLoading && <Loader />}
+            <Grid
+              container
+              spacing={2}
+              justifyContent="left"
+              alignItems="center"
+            >
+              {locationsList && locationsList.length > 0 ? (
+                locationsList?.map((location) => (
+                  <Grid item xs={6} lg={4} xl={3}>
+                    <LocationItem location={location} key={location.id} />
+                  </Grid>
+                ))
+              ) : (
+                <Grid item className={styles.noLocationsWrapper}>
+                  <Typography variant="h4" mb={3}>
+                    Couldn't find what you're looking for? Don't worry!
+                    <br /> Let us know, and we'll hunt it down for you.
+                  </Typography>
+                  <Button variant="contained" onClick={handleButtonClick}>
+                    Request Location Addition
+                  </Button>
+                  {successMessage && (
+                    <Typography variant="body1">{successMessage}</Typography>
+                  )}
+                  <Typography variant="h6" mt={3}>
+                    if we locate your desired location, we'll make sure to add
+                    it to our site for everyone to discover!"
+                  </Typography>
+                </Grid>
               )}
-              <Typography variant="h6" mt={3}>
-                if we locate your desired location, we'll make sure to add it to
-                our site for everyone to discover!"
-              </Typography>
             </Grid>
-          )}
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
--- src/services/apiLocationRequests.ts
+++ src/services/apiLocationRequests.ts
@@ -15,3 +15,42 @@ export const addLocationRequest = async (input: string): Promise<unknown> => {
     }
   }
 };
+
+export const getAllLocationRequests = async (): Promise<
+  {
+    location: string;
+    id: string;
+  }[]
+> => {
+  try {
+    const response = await api.get(`/location-req/`);
+    const resData = response.data;
+    return resData;
+  } catch (error: any) {
+    if (error.response && error.response.data && error.response.data.message) {
+      throw new Error(error.response.data.message);
+    } else {
+      throw new Error("An error occurred");
+    }
+  }
+};
+
+export const deleteLocationRequests = async (
+  id: string
+): Promise<
+  {
+    id: string;
+  }[]
+> => {
+  try {
+    const response = await api.delete(`/location-req/${id}`);
+    const resData = response.data;
+    return resData;
+  } catch (error: any) {
+    if (error.response && error.response.data && error.response.data.message) {
+      throw new Error(error.response.data.message);
+    } else {
+      throw new Error("An error occurred");
+    }
+  }
+};
